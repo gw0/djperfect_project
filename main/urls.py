eec4f606afe79a4137d42135d637d91f613d9c2f
gw0 [http://gw.tnode.com/] <gw.2012@tnode.com>
2012-05-01 15:13:20 +0200

    Added serving of `static` and `media` files in debug mode.

--- main/urls.py
+++ main/urls.py
@@ -3,15 +3,23 @@
 URL dispatcher config for {{ project_name|title }} Django project.
 """
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += patterns('',
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+    # url(r'^{{ project_name }}/', include('{{ project_name }}.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
