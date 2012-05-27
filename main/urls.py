73c7c0228017306fa42cbf2698ced85f59f9db03
gw0 [http://gw.tnode.com/] <gw.2012@tnode.com>
2012-05-27 22:54:48 +0200

    Code improvements to special URL pattern directives in debug mode.

--- main/urls.py
+++ main/urls.py
@@ -2,18 +2,18 @@
 """
 URL dispatcher config for {{ project_name|title }} Django project.
 """
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include, url, static
 from django.contrib import admin
 from django.conf import settings
-from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 admin.autodiscover()
 
 urlpatterns = []
 
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Special directives that should be handled by production web server
+    #urlpatterns += staticfiles_urlpatterns()  # already by default
+    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += patterns('',
         url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': settings.STATIC_URL+'img/favicon.ico'}),
         url(r'^robots\.txt$', 'django.views.generic.simple.redirect_to', {'url': settings.STATIC_URL+'robots.txt'}),
